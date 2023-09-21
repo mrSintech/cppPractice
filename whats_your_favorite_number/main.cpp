18b612eedb9c94cbcfa645759a0c92fc1d817041
Saeid Aghashahi <imsintech@gmail.com>
2023-09-21 18:30:29 +0330

    whats_your_favorite_number

--- whats_your_favorite_number/main.cpp
+++ whats_your_favorite_number/main.cpp
@@ -5,5 +5,6 @@ int main() {
     std::cout << "Enter your favorite Number between 0 and 100: ";
     std::cin >> num;
     std::cout << num;
+    std::cout << "? Amazing, That's my favorite number too." << std::endl;
     return 0;
 }
